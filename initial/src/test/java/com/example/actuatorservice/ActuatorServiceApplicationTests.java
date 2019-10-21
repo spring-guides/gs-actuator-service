ae62d0c7005efecaa7233a38deb0f807be541b38
Jay Bryant <jbryant@pivotal.io>
2019-10-21 13:50:35 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring 2.2, which required upgrading JUnit to version 5 and upgrading the Gradle wrappers.

---
+++ initial/src/test/java/com/example/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
