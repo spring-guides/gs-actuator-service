2568b948ecd72d1450e92ca3b768b4f7f1be0edd
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:07:33 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
+++ complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/hello-world")
-    @ResponseBody
-    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+	@GetMapping("/hello-world")
+	@ResponseBody
+	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 
 }
