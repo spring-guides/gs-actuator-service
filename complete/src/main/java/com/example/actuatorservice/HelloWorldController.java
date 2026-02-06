52a0e1890b65b4ab1a57835486a4978c95701d3b
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-06 14:28:56 +0100

    Polish
    See gh-36
    Closes gh-35

--- complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
+++ complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -2,19 +2,17 @@ package com.example.actuatorservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello-world")
-	@ResponseBody
 	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
