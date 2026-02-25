d7c47d19554b4cdd318be56b7dd5437f1835f4e2
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2026-02-25 14:07:14 -0700

    Update to Boot 4
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- complete/src/test/java/com/example/actuatorservice/HelloWorldApplicationTests.java
+++ complete/src/test/java/com/example/actuatorservice/HelloWorldApplicationTests.java
@@ -20,13 +20,13 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalManagementPort;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
@@ -36,32 +36,42 @@ import static org.assertj.core.api.BDDAssertions.then;
  * @author Dave Syer
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {"management.port=0"})
+@TestPropertySource(properties = {"management.server.port=0"})
 public class HelloWorldApplicationTests {
 
 	@LocalServerPort
 	private int port;
 
-	@Value("${local.management.port}")
+	@LocalManagementPort
 	private int mgt;
 
 	@Autowired
-	private TestRestTemplate testRestTemplate;
+	private RestClient.Builder restClientBuilder;
 
 	@Test
 	public void shouldReturn200WhenSendingRequestToController() throws Exception {
+		RestClient restClient = this.restClientBuilder
+				.baseUrl("http://localhost:" + this.port)
+				.build();
 		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
-				"http://localhost:" + this.port + "/hello-world", Map.class);
+		ResponseEntity<Map> entity = restClient.get()
+				.uri("/hello-world")
+				.retrieve()
+				.toEntity(Map.class);
 
 		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 	@Test
 	public void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+		RestClient restClient = this.restClientBuilder
+				.baseUrl("http://localhost:" + this.mgt)
+				.build();
 		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
-				"http://localhost:" + this.mgt + "/actuator", Map.class);
+		ResponseEntity<Map> entity = restClient.get()
+				.uri("/actuator")
+				.retrieve()
+				.toEntity(Map.class);
 		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
