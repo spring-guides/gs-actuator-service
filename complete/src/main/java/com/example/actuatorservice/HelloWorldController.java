f1ce7e3a41e8f80711bdd4a58bead825e5597f9d
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-27 08:53:03 +0100

    Polish code formatting and initial test structure
    start.spring.io generates a ActuatorServiceApplicationTests and yet
    complete rename that to HelloWorldApplicationTests. It also uses tabs
    and the existing code was using spaces.
    
    See gh-42

--- complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
+++ complete/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -1,20 +1,21 @@
 package com.example.actuatorservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloWorldController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
 
-	@GetMapping("/hello-world")
-	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello-world")
+    public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 
 }
