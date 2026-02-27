f13ef53f6f8507e775b2fb167296b455e5a36847
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2026-02-27 08:53:03 +0100

    Polish code formatting and initial test structure
    start.spring.io generates a ActuatorServiceApplicationTests and yet
    complete rename that to HelloWorldApplicationTests. It also uses tabs
    and the existing code was using spaces.
    
    See gh-42

--- initial/src/main/java/com/example/actuatorservice/ActuatorServiceApplication.java
+++ initial/src/main/java/com/example/actuatorservice/ActuatorServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActuatorServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActuatorServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorServiceApplication.class, args);
+    }
 
 }
